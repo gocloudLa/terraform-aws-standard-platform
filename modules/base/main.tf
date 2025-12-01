041787030c365605d4400c3289b628a9c6586b68
Ignacio Bisio <ibisio@gocloud.la>
2025-12-01 12:53:09 -0300

    feat(base): add vpc feature

--- modules/base/main.tf
+++ modules/base/main.tf
@@ -1,6 +1,6 @@
 module "wrapper_vpc" {
-  source  = "gocloudLa/wrapper-vpc/aws"
-  version = "1.0.2"
+  source  = "gocloudLa/terraform-aws-wrapper-vpc?ref=feature/add-custom-names"
+  #version = "1.0.2"
 
   metadata = var.metadata
 
