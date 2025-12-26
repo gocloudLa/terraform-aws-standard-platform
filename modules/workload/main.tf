d51c8375c2d6bf4a93a9530e815e5be60c577622
Ignacio Bisio <ibisio@gocloud.la>
2025-12-26 17:34:00 -0300

    feat(workload): update ec2 instance source

--- modules/workload/main.tf
+++ modules/workload/main.tf
@@ -46,8 +46,8 @@ module "wrapper_lambda" {
 }
 
 module "wrapper_ec2_instance" {
-  source  = "gocloudLa/wrapper-ec2-instance/aws"
-  version = "0.1.1"
+  source  = "git::https://github.com/gocloudLa/terraform-aws-wrapper-ec2-instance.git?ref=feature/add-custom-names" #"gocloudLa/wrapper-ec2-instance/aws"
+  # version = "0.1.1"
 
   metadata = var.metadata
 
