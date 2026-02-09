f542e142f43533e7136f617b785da00edf69c914
Ignacio Bisio <ibisio@gocloud.la>
2026-02-09 14:40:24 -0300

    feat(workload): update ec2 module version to 0.1.3

--- modules/workload/main.tf
+++ modules/workload/main.tf
@@ -47,7 +47,7 @@ module "wrapper_lambda" {
 
 module "wrapper_ec2_instance" {
   source  = "gocloudLa/wrapper-ec2-instance/aws"
-  version = "0.1.2"
+  version = "0.1.3"
 
   metadata = var.metadata
 
