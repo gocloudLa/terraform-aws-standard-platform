374ade5d5a6a1f8e9eb3d116a5b04cfa689f47dd
Federico Midaglia <fmidaglia@gocloud.la>
2026-04-04 16:06:29 -0300

    feat(base): add outputs

--- modules/base/output.tf
+++ modules/base/output.tf
@@ -1,3 +1,18 @@
+output "wrapper_vpc" {
+  description = ""
+  value       = module.wrapper_vpc
+}
+
+output "wrapper_tgw" {
+  description = ""
+  value       = module.wrapper_tgw
+}
+
+output "wrapper_vpn" {
+  description = ""
+  value       = module.wrapper_vpn
+}
+
 output "route53_zones" {
   description = "Name servers of Route53 zone"
   value       = module.wrapper_route53.route53_zones
