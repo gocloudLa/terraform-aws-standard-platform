579043ea4439850bf0eea22362dd9221674e0d3d
Federico Midaglia <fmidaglia@gocloud.la>
2026-04-21 13:53:00 -0300

    feat(modules): update modules ecs-service, rds, rds-aurora (#71)

--- modules/workload/main.tf
+++ modules/workload/main.tf
@@ -14,7 +14,7 @@ module "wrapper_static_site" {
 
 module "wrapper_ecs_service" {
   source  = "gocloudLa/wrapper-ecs-service/aws"
-  version = "1.4.2"
+  version = "1.4.3"
 
   metadata = var.metadata
 
